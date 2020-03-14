9de46c35d317a092757be8d0a74a5fc481a015f6
Leechay <lm16154@my.bristol.ac.uk>
2020-03-14 19:52:34 +0000

    Neighbours functionality complete and fully tested

--- ms_testing.c
+++ ms_testing.c
@@ -55,28 +55,70 @@ int identifyNeighbours(Space * grid, int index){
   int neighbourCount = 0;
 
   //inner square
-  for(int M = (X_GRID+1) ; M < ( (X_GRID) * (Y_GRID) ); M++) {
+  for(int M = 0 ; M < ( (X_GRID) * (Y_GRID) ); M++) {
     traversalIndex = M % Y_GRID;
 
     //checks if pointing at top or bottom row value
     if( ((traversalIndex != 0) && (traversalIndex != (Y_GRID-1))) && ( (M < 21 ) && (M > 3) ) ) {
       grid[M].adjacentNeighbours = 0;
-      grid[M].adjacentNeighbours = grid[M-1].holdsMine + grid[M+1].holdsMine + grid[ (M + X_GRID) ].holdsMine
-                                 + grid[  (M - X_GRID) ].holdsMine + grid[ (M-1) + X_GRID ].holdsMine
-                                 + grid[ (M-1) - X_GRID ].holdsMine + grid[ (M+1) + X_GRID ].holdsMine
-                                 + grid[ (M+1) - X_GRID ].holdsMine;
+      grid[M].adjacentNeighbours = grid[M-1].holdsMine + grid[M+1].holdsMine + grid[ (M + Y_GRID) ].holdsMine
+                                 + grid[  (M - Y_GRID) ].holdsMine + grid[ (M-1) + Y_GRID ].holdsMine
+                                 + grid[ (M-1) - Y_GRID ].holdsMine + grid[ (M+1) + Y_GRID ].holdsMine
+                                 + grid[ (M+1) - Y_GRID ].holdsMine;
       printf("(Index: %d, Adjacent Neighbours: %d)\n", M, grid[M].adjacentNeighbours);
     }
+
+    //four corners
+    grid[0].adjacentNeighbours = grid[Y_GRID].holdsMine + grid[(Y_GRID + 1)].holdsMine + grid[1].holdsMine;  //top right
+
+    grid[(Y_GRID - 1)].adjacentNeighbours = grid[ (Y_GRID - 1) - 1 ].holdsMine + grid[(Y_GRID - 1) + Y_GRID].holdsMine
+                                          + grid[(Y_GRID - 1) + Y_GRID - 1].holdsMine; //bottom left
+
+    grid[(X_GRID * Y_GRID - 1)].adjacentNeighbours = grid[ (X_GRID * Y_GRID - 1) - 1 ].holdsMine + grid[ (X_GRID * Y_GRID - 1) - Y_GRID].holdsMine
+                                               + grid[ (X_GRID * Y_GRID - 1) - (Y_GRID + 1)].holdsMine; //bottom right
+
+    grid[ (X_GRID * (Y_GRID - 1)) ].adjacentNeighbours = grid[ (X_GRID * (Y_GRID - 1)) + 1 ].holdsMine
+                                                       + grid[ (X_GRID * (Y_GRID - 1)) - Y_GRID].holdsMine
+                                                       + grid[ (X_GRID * (Y_GRID - 1)) - (Y_GRID) + 1].holdsMine; //bottom left
+
+    //top & bottom rows
+
+    //left & right columns
+
+    for(int K = 1; K < Y_GRID-1; ++K){
+      //left column
+      grid[K].adjacentNeighbours = grid[K-1].holdsMine + grid[K+1].holdsMine + grid[(K*Y_GRID)].holdsMine + grid[(K*Y_GRID) - 1].holdsMine
+                                 + grid[(K*Y_GRID) + 1].holdsMine;
+
+      //right column
+      grid[ (X_GRID * (Y_GRID - 1)) + K ].adjacentNeighbours = grid[ (X_GRID * (Y_GRID - 1)) + K + 1 ].holdsMine
+                                                             + grid[ (X_GRID * (Y_GRID - 1)) + K - 1 ].holdsMine
+                                                             + grid[ (X_GRID * (Y_GRID - 1)) + K - Y_GRID ].holdsMine
+                                                             + grid[ (X_GRID * (Y_GRID - 1)) + K - Y_GRID + 1 ].holdsMine
+                                                             + grid[ (X_GRID * (Y_GRID - 1)) + K - Y_GRID - 1 ].holdsMine;
+
+
+      //top row
+      grid[K*Y_GRID].adjacentNeighbours = grid[K*Y_GRID + 1].holdsMine + grid[(K+1)*Y_GRID].holdsMine + grid[(K-1)*Y_GRID].holdsMine
+                                        + grid[K*Y_GRID + 1 - Y_GRID].holdsMine + grid[K*Y_GRID + 1 + Y_GRID].holdsMine;
+
+
+
+      //bottom row
+      grid[(K+1)*(Y_GRID) - 1 ].adjacentNeighbours = grid[(K+1)*(Y_GRID) - 1 - 1].holdsMine
+                                                   + grid[(K+1)*(Y_GRID) - 1 - Y_GRID].holdsMine
+                                                   + grid[(K+1)*(Y_GRID) - 1 + Y_GRID].holdsMine
+                                                   + grid[(K+1)*(Y_GRID) - 1 - Y_GRID - 1].holdsMine
+                                                   + grid[(K+1)*(Y_GRID) - 1 + Y_GRID - 1].holdsMine;
+    }
+
+
+
     //index getter for testing
     if(M == index){
       neighbourCount = grid[M].adjacentNeighbours;
     }
   }
-  //four corners
-
-  //top & bottom rows
-
-  //left & right columns
   return neighbourCount;
 }
 
@@ -137,14 +179,33 @@ void test(){
 
 
   //center square
-  eqi(grid[13].adjacentNeighbours, identifyNeighbours(s,13));
-  eqi(grid[10].adjacentNeighbours, identifyNeighbours(s,10));
-  eqi(grid[18].adjacentNeighbours, identifyNeighbours(s,18));
-  eqi(grid[13].adjacentNeighbours, identifyNeighbours(s,13));
-  eqi(grid[16].adjacentNeighbours, identifyNeighbours(s,16));
+  int a,b,c,d,e;
+  a = identifyNeighbours(s,13);
+  b = identifyNeighbours(s,10);
+  c = identifyNeighbours(s,18);
+  d = identifyNeighbours(s,16);
+  e = identifyNeighbours(s,4);
+  eqi(grid[13].adjacentNeighbours, a);
+  eqi(grid[10].adjacentNeighbours, b);
+  eqi(grid[18].adjacentNeighbours, c);
+  eqi(grid[16].adjacentNeighbours, d);
 
-  //left column
-  eqi(grid[4].adjacentNeighbours, identifyNeighbours(s,4));
+  //corners
+  eqi(grid[4].adjacentNeighbours, e);
+  eqi(grid[0].adjacentNeighbours, identifyNeighbours(s,0));
+  eqi(grid[20].adjacentNeighbours, identifyNeighbours(s,20));
+  eqi(grid[24].adjacentNeighbours, identifyNeighbours(s,24));
+
+  //rows and columns
+  eqi(grid[1].adjacentNeighbours, identifyNeighbours(s,1));
+  eqi(grid[3].adjacentNeighbours, identifyNeighbours(s,3));
+  eqi(grid[21].adjacentNeighbours, identifyNeighbours(s,21));
+  eqi(grid[23].adjacentNeighbours, identifyNeighbours(s,23));
+  eqi(grid[10].adjacentNeighbours, identifyNeighbours(s,10));
+  eqi(grid[5].adjacentNeighbours, identifyNeighbours(s,5));
+  eqi(grid[14].adjacentNeighbours, identifyNeighbours(s,14));
+  eqi(grid[19].adjacentNeighbours, identifyNeighbours(s,19));
+
 
   //right column
   printf("Tests passed: %d\n", eqi(0, 0) - 1);
